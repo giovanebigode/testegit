a2ab519f076f9cc95bd8bca1480a2381eb193ce5
giovanne <giovanne@gmail.com>
2024-02-28 21:44:31 -0300

    commit 01

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+   function calcdobro (){
+       $dobro = $num * 2;
+       return $dobro;
+    }
+   function calctriplo($num){
+       $triplo = $num * 3;
+       return $triplo;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    ?>
